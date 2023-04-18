6b45b7c8d8a5377bbb365379112e76c307f2097b
jay-amin-tops <jay.amin.tops@gmail.com>
2023-04-18 14:49:42 +0530

    data

---
+++ PHP/01Intro_18April.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h2>PHP</h2>
+<p>Hypertext Pre-Processor</p>
+<p>Eralier its called as Personal Home Page </p>
+<p>Rusmus Lerdorf invented in 1994 and published in 1995</p>
+<p>PHP is server side scripting lang with fast res</p>
+<p>earliier target is just uploading personal information / resume </p>
+<p>later on Andi Gutmans and Zeev Suraski invented ZEND engine and adopted OOPS </p>
+<p>CMS and Frameworks are main straingth of PHP</p>
+
+<h2>CMS</h2>
+<ul>
+    <li>Wordpress</li>
+    <li>Mangento</li>
+    <li>Drupal</li>
+</ul>
+
+<ul>
+    <li>Laravel</li>
+    <li>Codeigniter</li>
+    <li>Symphony</li>
+    <li>Cake</li>
+    <li>Yii</li>
+</ul>
+
+</body>
+</html>
---
+++ PHP/03variableDeclaration.php
@@ -0,0 +1,9 @@
+<?php
+// comment => A comment in PHP code is a line that is not executed as a part of the program. Its only purpose is to be read by someone who is looking at the code. Comments can be used to: Let others understand your code. 
+
+$username = "Madhav"; // correct;
+$username12 = "value"; // correct
+// $user name = "value" // wrong
+// $@#$%^&*()user name = "value" // wrong
+// $123user name = "value" // wrong
+echo "data";
