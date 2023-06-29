5bfcb11233765013f948b490e6aa12f7afe55f8f
jay-amin-tops <jay.amin.tops@gmail.com>
2023-06-29 14:42:20 +0530

    data

---
+++ PHP/MVCRev/Controller/Controller.php
@@ -0,0 +1,97 @@
+<?php
+session_start();
+include_once("Model/Model.php");
+class Controller extends Model{
+    public $baseURL = "http://localhost/laravel/04April_laravel_shailesh_TTS2/PHP/MVCRev/Assets/";
+    public function __construct() {
+        ob_start();
+        parent::__construct();
+        
+        // parent::__construct("april04");
+        if ( isset($_SERVER['PATH_INFO']) ) {
+            switch ($_SERVER['PATH_INFO']) {
+                case '/home':
+                    // echo "called";
+                    include_once("Views/header.php");
+                    include_once("Views/main.php");
+                    include_once("Views/footer.php");
+                    break;
+                case '/login':
+                    include_once("Views/loginpage.php");
+                    if(isset($_POST['btn-login'])) {
+                        $LoginRes = $this->login($_POST['username'],$_POST['password']);
+                        if ($LoginRes['Code'] == 1) {
+                            
+                            // echo "<pre>";
+                            // print_r($LoginRes['Data']->role_id);
+                            $_SESSION['userData']=$LoginRes['Data'];
+                            if ($LoginRes['Data']->role_id == 1) {
+                                # code...
+                                echo "<script> alert('Hello admin'); window.location.href='admindashboard' </script>";
+                            } else {
+                                echo "<script> alert('Login success'); window.location.href='home' </script>";
+                                # code...
+                            }
+                            
+
+                        }else{
+                            echo "<script> alert('invalid user') </script>";
+                        }
+                        echo "</pre>";
+                    }
+                    break;
+
+                case '/registration':
+                    // echo "called";
+                    // include_once("Views/header.php");
+                    include_once("Views/registration.php");
+                    if (isset($_POST['registration'])) {
+                        array_pop($_POST);
+                        $Hobbies = implode(",",$_POST['chk']);
+                        unset($_POST['chk']);
+                        $Data = array_merge($_POST,array("hobbies"=>$Hobbies));
+                        // echo "<pre>";
+                        // print_r($Data);
+                        // echo "</pre>";
+                        
+                        // $Data = array("username"=>"test",
+                        //                "password"=>"123", 
+                        //                "email"=>"test@mail.com", 
+                        //                "gender"=>"Male", 
+                        //                "mobile"=>"9879878955", 
+                        //                "hobbies"=>"Circket,music", 
+                        //                "city"=>"1", 
+                        //                "profile_pic"=>"default.jpg");
+                        // $Data = array("username"=>$_POST['username'],
+                        //                "password"=>$_POST['password'], 
+                        //                "email"=>$_POST[''], 
+                        //                "gender"=>$_POST[''], 
+                        //                "mobile"=>"9879878955", 
+                        //                "hobbies"=>"Circket,music", 
+                        //                "city"=>"1", 
+                        //                "profile_pic"=>"default.jpg");
+                        $Res = $this->insert("users",$Data);
+                        echo "<pre>";
+                        print_r($Res['Code']);
+                        if ($Res['Code'] == 1) {
+                            header("location:login");
+                        }
+                    }
+                    // include_once("Views/footer.php");
+                    break;
+                case '/admindashboard':
+                    echo "Welcome to admin dashboard";
+                    break;
+                default:
+                    # code...
+                    break;
+            }
+        }else{
+            header("location:home");
+        }
+        ob_flush();
+    }
+}
+
+$Controller = new Controller;
+?>
---
+++ PHP/MVCRev/Model/Model.php
@@ -0,0 +1,58 @@
+<?php 
+
+class Model{
+    public function __construct(protected $db="") {
+        try {
+            $this->db = new mysqli("localhost","root","","april04"); 
+        } catch (\Exception $e) {
+            $ErrorMsg = PHP_EOL."Error Date Time >> ".date('d-m-Y h:i:s A').PHP_EOL."Error Msg >> ".$e->getMessage().PHP_EOL;
+            echo $ErrorMsg;
+            if (!file_exists('log')) {
+                mkdir("log");
+            }
+            $FileName = date('d_m_Y');
+            file_put_contents("log/".$FileName."_log.txt",$ErrorMsg,FILE_APPEND);
+        }
+    }
+    function insert($tbl,$data) {
+        // echo "called";
+        // print_r($this->db);
+        $clms = implode(",",array_keys($data)) ;
+        $vals = implode("','",$data) ;
+        $SQL = "INSERT INTO $tbl($clms)VALUES('$vals')";
+        $SQLEx = $this->db->query($SQL);
+        // print_r($SQLEx);
+        if ($SQLEx > 0) {
+            $ResData['Data'] = "1";
+            $ResData['Msg'] = "Success";
+            $ResData['Code'] = "1";
+        }else{
+            $ResData['Data'] = "0";
+            $ResData['Msg'] = "try again";
+            $ResData['Code'] = "0";
+
+        }
+        return $ResData;
+    }
+    function login($uname,$pass) {
+        $SQL = "SELECT * FROM users WHERE password='$pass' AND (email='$uname' OR mobile='$uname' OR username='$uname') ";
+        $SQLEx = $this->db->query($SQL);
+        // echo "<pre>";
+        // print_r($SQLEx);
+        // echo "</pre>";
+        if ($SQLEx->num_rows > 0) {
+            $ResData['Data'] = $SQLEx->fetch_object();
+            $ResData['Msg'] = "Success";
+            $ResData['Code'] = "1";
+        }else{
+            $ResData['Data'] = "0";
+            $ResData['Msg'] = "try again";
+            $ResData['Code'] = "0";
+
+        }
+        return $ResData;
+    }
+}
+
+
+?>
